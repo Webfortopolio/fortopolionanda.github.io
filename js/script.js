bc7e843466d943c40d616f8b5611717dcda5cd78
Webfortopolio <nanda.kurniawan1208@gmail.com>
2021-12-19 17:33:08 +0700

    upload web fortopolio nanda

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('nav').toggleClass('nav-toggle');
+    });
+
+    $('nav ul li a').click(function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('nav').removeClass('nav-toggle');
+    });
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() < 20){
+            $('header').css('box-shadow','none');
+        }else{
+            $('header').css('box-shadow','0 .2rem .5rem #333');
+        }
+    });
+
+});
